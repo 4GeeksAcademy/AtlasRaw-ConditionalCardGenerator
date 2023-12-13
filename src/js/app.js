159be7a6cea34102bc204fdac519559efe7b2980
atlasraw <144444822+atlasraw@users.noreply.github.com>
2023-12-13 20:26:31 +0000

    finish

--- src/js/app.js
+++ src/js/app.js
@@ -33,15 +33,29 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${variables.name ? variables.name : "Atlas"} ${
+    variables.lastName ? variables.lastName : "Raw"
+  }</h1>
+          <h2>${variables.role ? variables.role : "Developer"}</h2>
+          <h3>${variables.city ? variables.city : "Barcelona"}, ${
+    variables.country ? variables.country : "España"
+  } </h3>
+          <ul class= ${variables.socialMediaPosition}>
+          
+            <li><a href="https://twitter.com/${
+              variables.twitter ? variables.twitter : "SpoingusS"
+            }"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${
+              variables.github ? variables.github : "atlasraw"
+            }"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://www.linkedin.com/in/${
+              variables.linkedin ? variables.linkedin : "Mathias Asenjo"
+            }"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://www.instagram.com/${
+              variables.instagram ? variables.instagram : "atlasraw.az"
+            }"><i class="fab fa-instagram"></i></a></li>
           </ul>
+
         </div>
     `;
 }
@@ -54,9 +68,10 @@ window.onload = function() {
     // if includeCover is true the algorithm should show the cover image
     includeCover: true,
     // this is the image's url that will be used as a background for the profile cover
-    background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
+    background:
+      "https://i.pinimg.com/originals/38/04/fb/3804fb64b5588ee680ae948b9b2778f3.png",
     // this is the url for the profile avatar
-    avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
+    avatarURL: "https://avatars.githubusercontent.com/u/144444822?v=4",
     // social media bar position (left or right)
     socialMediaPosition: "position-left",
     // social media usernames
